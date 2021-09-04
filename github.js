b86ab76e1820433c2f9b6eee60ef3baeed3ba95d
TediashviliGiorgi <personalmailgt@yahoo.com>
2021-09-05 00:20:02 +0400

    initial commit

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = '649315c1b66448996dcb';
     this.client_secret = 'c09cea9cd0f6a4bb87885d01d81cba74ad0b118b';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
